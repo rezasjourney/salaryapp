c53c43d0d39222157460a935635adf218cfd45dd
Reza Fazeli <rezafazeli@gmail.com>
2019-11-29 14:30:13 -0500

    Load trained model and return salary predictions

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,11 @@
-from app import app
+from app import app, utils
+from model import train
+
+import joblib
+from pathlib import Path
+from os.path import join
+
+from flask import jsonify
 
 @app.route('/')
 def index():
@@ -8,5 +15,18 @@ def index():
 def predict(company_name, posting_id):
     base_url = 'https://jobs.lever.co/'
     url = base_url + company_name + '/' + posting_id
-    return url
+    description = utils.extract_text(url)
+    description = utils.clean_text(description)
+
+    app_dir = Path(__file__).resolve().parents[1]
+    model, vectorizer = train.load(join(app_dir, 'naive_model.joblib'))
+
+    prediction = model.predict(vectorizer.transform([description]))[0]
+
+    output = {
+        'URL': url,
+        'Prediction': '${:,.2f}'.format(round(prediction, 2))
+    }
+    
+    return jsonify(output)
    
